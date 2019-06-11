6f8785c4f39dc0a27cde137f959c5984323ee8d3
Andrey Manzharov <andreyrmg@gmail.com>
2019-06-11 09:10:54 +0300

    fish: default shell

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,6 @@
 set -x LANG en_US.UTF-8
 set -x PATH $HOME/.local/bin $HOME/.cargo/bin $PATH
 
+set -x LESS "$LESS -F -X -S"
+
 set -x HG_LOG_TEMPLATE "{label('yellow', rev)} {label('red', shortest(node, 6))} -{label('yellow', ifeq(branch, 'default', '', ' {branch}'))}{label('yellow', if(tags, ' (tags: {join(tags, ', ')})'))}{label('yellow', if(bookmarks, ' [bookmarks: {join(bookmarks, ', ')}]'))} {desc|strip|firstline} {label('green', '({date|age})')} {label('blue', '<{author|person}>')}\n"
--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,21 +1,21 @@
 function fish_prompt --description 'Write out the prompt'
-	set -l last_status $status
+  set -l last_status $status
 
-    echo
+  echo
 
-    # PWD
-    set_color $fish_color_cwd
-    echo -n (prompt_pwd)
-    set_color normal
+  # PWD
+  set_color $fish_color_cwd
+  echo -n (prompt_pwd)
+  set_color normal
 
-    __terlar_git_prompt
-    __fish_hg_prompt
-    echo
+  __terlar_git_prompt
+  __fish_hg_prompt
+  echo
 
-    if not test $last_status -eq 0
-        set_color $fish_color_error
-    end
+  if not test $last_status -eq 0
+    set_color $fish_color_error
+  end
 
-    echo -n '> '
-    set_color normal
+  echo -n '> '
+  set_color normal
 end
