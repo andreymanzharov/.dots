7abd1184fa15f66d826a02136b7aec7e83f7a763
Andrey Manzharov <andreyrmg@gmail.com>
2020-02-06 16:39:57 +0300

    fish: setup base16 colors

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,6 +5,11 @@ if status is-login
   set -xp CDPATH . /x
 end
 
+if status is-interactive
+  set BASE16_SHELL "$HOME/.config/base16-shell/"
+  source "$BASE16_SHELL/profile_helper.fish"
+end
+
 set -x LESS "$LESS -F -X -S"
 
 set -x HG_LOG_TEMPLATE "{label('yellow', rev)} {label('red', shortest(node, 6))} -{label('yellow', ifeq(branch, 'default', '', ' {branch}'))}{label('yellow', if(tags, ' (tags: {join(tags, ', ')})'))}{label('yellow', if(bookmarks, ' [bookmarks: {join(bookmarks, ', ')}]'))} {desc|strip|firstline} {label('green', '({date|age})')} {label('blue', '<{author|person}>')}\n"
