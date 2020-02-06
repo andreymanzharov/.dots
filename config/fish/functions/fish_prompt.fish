dae0f47b7f8abb01fe83e2c4b009401152b66a67
Andrey Manzharov <andreyrmg@gmail.com>
2020-02-06 16:43:52 +0300

    fish: show user in prompt if not andrey

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -12,10 +12,16 @@ function fish_prompt --description 'Write out the prompt'
   __fish_hg_prompt
   echo
 
+  if not test "$USER" = "andrey"
+    set_color brred
+    echo -n $USER
+    set_color normal
+  end
+
   if not test $last_status -eq 0
     set_color $fish_color_error
   end
 
-  echo -n '> '
+  echo -n '$ '
   set_color normal
 end
