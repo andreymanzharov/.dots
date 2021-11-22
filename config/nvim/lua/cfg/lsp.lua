2da8b4f40b5e534b8a40bf19eb247760c8aa5731
Andrey Manzharov <andreyrmg@gmail.com>
2021-11-22 10:19:51 +0300

    nvim: fix indentation

--- config/nvim/lua/cfg/lsp.lua
+++ config/nvim/lua/cfg/lsp.lua
@@ -53,15 +53,15 @@ local function configure_lsp()
   }
 
   vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    underline = false,
-    virtual_text = {
-      spacing = 5,
-      prefix = '~',
-    },
-    signs = true,
-    update_in_insert = false,
-  }
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+      underline = false,
+      virtual_text = {
+        spacing = 5,
+        prefix = '~',
+      },
+      signs = true,
+      update_in_insert = false,
+    }
   )
 end
 
