1727d09c9bfeb63499e5f5f15e465dc2d4708239
Andrey Manzharov <andreyrmg@gmail.com>
2022-02-14 09:21:13 +0300

    fish: refactor environment variables

--- config/fish/conf.d/1-sway.fish
+++ config/fish/conf.d/1-sway.fish
@@ -1,6 +1,10 @@
 if status is-login
   set -l tty1 (tty)
   if test "$tty1" = "/dev/tty1"
+    set -x MOZ_ENABLE_WAYLAND 1
+    set -x QT_QPA_PLATFORM wayland
+    set -x QT_WAYLAND_DISABLE_WINDOWDECORATION 1
+    set -x _JAVA_AWT_WM_NONREPARENTING 1
     exec sway
   end
 end
