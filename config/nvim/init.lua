59d5abb1751d7b68b813f2c1da2d8646397d8fc9
Andrey Manzharov <andreyrmg@gmail.com>
2022-03-12 17:07:27 +0300

    nvim: highlight yanked object

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -15,8 +15,15 @@ vim.cmd[[
 vim.cmd[[autocmd BufWritePre * :%s/\s\+$//e]]
 
 vim.cmd[[
-autocmd UIEnter *
-      \ let g:neovide_cursor_vfx_mode = "pixiedust" |
-      \ set guifont=Iosevka\ Term:h12 |
-      \
+  autocmd UIEnter *
+        \ let g:neovide_cursor_vfx_mode = "pixiedust" |
+        \ set guifont=Iosevka\ Term:h12 |
+        \
+]]
+
+vim.cmd[[
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+  augroup end
 ]]
