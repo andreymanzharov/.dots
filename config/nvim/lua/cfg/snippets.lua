a5c73b1be497910a92de79a3affbe99ea056adda
Andrey Manzharov <andreyrmg@gmail.com>
2022-08-12 09:49:00 +0300

    nvim: add basic mappings for luasnip

--- config/nvim/lua/cfg/snippets.lua
+++ config/nvim/lua/cfg/snippets.lua
@@ -89,6 +89,21 @@ ls.add_snippets('rust', {
   key = 'rust',
 })
 
+vim.keymap.set('i', '<c-k>', function ()
+  if ls.expand_or_jumpable() then
+    ls.expand_or_jump()
+  end
+end, { silent = true })
+vim.keymap.set('s', '<c-k>', function ()
+  if ls.jumpable(1) then
+    ls.jump(1)
+  end
+end, { silent = true })
+vim.keymap.set({'i', 's'}, '<c-j>', function ()
+  if ls.jumpable(-1) then
+    ls.jump(-1)
+  end
+end, { silent = true })
 vim.keymap.set('i', '<c-l>', function ()
   if ls.choice_active() then
     ls.change_choice(1)
