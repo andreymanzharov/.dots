80036e6ddfc06174f2eb4d5b918ff75ebb8b0782
Andrey Manzharov <amanzharov@yandex.ru>
2023-05-29 07:32:44 +0300

    nvim: use `Iosevka Term` anyway

--- config/nvim/plugin/neovide.lua
+++ config/nvim/plugin/neovide.lua
@@ -4,8 +4,4 @@ end
 
 vim.g.neovide_transparency = 0.9
 vim.g.neovide_cursor_vfx_mode = "railgun"
-if vim.fn.has('macunix') == 1 then
-  vim.opt.guifont = "Iosevka Term"
-else
-  vim.opt.guifont = "monospace:e-subpixelantialias:#h-slight"
-end
+vim.opt.guifont = "Iosevka Term"
