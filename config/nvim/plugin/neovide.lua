f7b1b22726c218fa10a2d198a44744f9b1f354d8
Andrey Manzharov <amanzharov@yandex.ru>
2023-06-21 14:46:53 +0300

    neovide: set refresh rate

--- config/nvim/plugin/neovide.lua
+++ config/nvim/plugin/neovide.lua
@@ -2,6 +2,7 @@ if not vim.g.neovide then
   return
 end
 
+vim.g.neovide_refresh_rate = 60
 vim.g.neovide_transparency = 0.9
 vim.g.neovide_cursor_vfx_mode = "railgun"
 vim.opt.guifont = "Iosevka Term"
