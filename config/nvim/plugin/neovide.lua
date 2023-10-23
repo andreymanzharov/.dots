07ee3526f848ed5532e2dd46ac79acde637701e9
Andrey Manzharov <amanzharov@yandex.ru>
2023-10-23 14:41:49 +0300

    neovide: remote cursor trail

--- config/nvim/plugin/neovide.lua
+++ config/nvim/plugin/neovide.lua
@@ -4,5 +4,6 @@ end
 
 vim.g.neovide_refresh_rate = 60
 vim.g.neovide_transparency = 0.9
+vim.g.neovide_cursor_trail_size = 0
 vim.g.neovide_cursor_vfx_mode = "railgun"
 vim.opt.guifont = "Iosevka Term"
