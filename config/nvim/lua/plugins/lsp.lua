bd394bae26d10cfc083143afaaf3a20dcd446bf0
Andrey Manzharov <amanzharov@yandex.ru>
2024-01-04 12:01:12 +0300

    nvim: update `lua_ls` configuration

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -13,23 +13,27 @@ return {
         pylsp = {},
         ocamllsp = {},
         lua_ls = {
-          settings = {
-            Lua = {
-              runtime = {
-                version = 'LuaJIT',
-              },
-              diagnostics = {
-                globals = { 'vim' },
-              },
-              workspace = {
-                library = vim.api.nvim_get_runtime_file("", true),
-                checkThirdParty = false,
-              },
-              telemetry = {
-                enable = false,
-              },
-            },
-          },
+          on_init = function(client)
+            local path = client.workspace_folders[1].name
+            if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+              client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+                Lua = {
+                  runtime = {
+                    version = 'LuaJIT'
+                  },
+                  workspace = {
+                    checkThirdParty = false,
+                    library = {
+                      vim.env.VIMRUNTIME
+                      -- "${3rd}/luv/library"
+                      -- "${3rd}/busted/library",
+                    }
+                  }
+                }
+              })
+            end
+            client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+          end,
         }
       }
     },
