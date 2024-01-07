feeb9baef0606e211fc574c187a595f137e08d5c
Andrey Manzharov <amanzharov@yandex.ru>
2024-01-07 17:45:41 +0300

    nvim: add scale factor mappings for Neovide

--- config/nvim/plugin/neovide.lua
+++ config/nvim/plugin/neovide.lua
@@ -5,5 +5,13 @@ end
 vim.g.neovide_refresh_rate = 60
 vim.g.neovide_transparency = 0.9
 vim.g.neovide_cursor_trail_size = 0
-vim.g.neovide_cursor_vfx_mode = "railgun"
-vim.opt.guifont = "Iosevka Term"
+vim.g.neovide_cursor_vfx_mode = 'railgun'
+vim.opt.guifont = 'Iosevka Term'
+
+local function set_scale_factor(scale)
+  vim.g.neovide_scale_factor = scale
+  vim.cmd.redraw { bang = true }
+end
+vim.keymap.set('n', '<d-0>', function() set_scale_factor(1) end)
+vim.keymap.set('n', '<d-->', function() set_scale_factor(vim.g.neovide_scale_factor - .1) end)
+vim.keymap.set('n', '<d-=>', function() set_scale_factor(vim.g.neovide_scale_factor + .1) end)
