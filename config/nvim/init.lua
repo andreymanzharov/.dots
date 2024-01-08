c1fb5d1e6d91d28533b1145ae95b05be494fbc98
Andrey Manzharov <amanzharov@yandex.ru>
2024-01-08 16:55:31 +0300

    nvim: change leader to space

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,12 +1,12 @@
-vim.g.mapleader = ","
+vim.g.mapleader = ' '
 
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*',
   command = [[%substitute/\s\+$//e]],
 })
 
