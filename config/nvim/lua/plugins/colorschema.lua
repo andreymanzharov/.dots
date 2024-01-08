bf2ea42ed4289ec9171e4b3e7521f4a28ca04d8d
Andrey Manzharov <amanzharov@yandex.ru>
2024-01-08 16:41:44 +0300

    nvim: switch to `catppuccin` color scheme

--- config/nvim/lua/plugins/colorschema.lua
+++ config/nvim/lua/plugins/colorschema.lua
@@ -1,37 +1,20 @@
 return {
   {
-    'RRethy/nvim-base16',
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000,
-    config = function()
-      vim.opt.background = 'dark'
-      vim.opt.termguicolors = true
-
-      vim.api.nvim_create_autocmd("ColorScheme", {
-        group = vim.api.nvim_create_augroup("LspSemanticHighlight", {}),
-        callback = function()
-          for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
-            vim.api.nvim_set_hl(0, group, {})
-          end
-          vim.api.nvim_set_hl(0, 'WinSeparator', { link = 'LineNr' })
-        end,
-      })
-
-      require 'base16-colorscheme'.with_config {
-        telescope = false,
-      }
-
-      if vim.env.BASE16_THEME and vim.g.colors_name ~= vim.env.BASE16_THEME then
-        vim.cmd.colorscheme('base16-' .. vim.env.BASE16_THEME)
-      else
-        vim.cmd.colorscheme 'base16-default-dark'
-      end
+    opts = {
+    },
+    config = function(_, opts)
+      require 'catppuccin'.setup(opts)
+      vim.cmd.colorscheme 'catppuccin'
     end,
   },
   {
     'nvim-lualine/lualine.nvim',
     opts = {
       options = {
-        icons_enabled = true,
+        theme = 'catppuccin',
       },
       sections = {
         lualine_c = { {
