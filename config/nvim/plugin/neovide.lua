7e9fbde37770f4228a5a9624c146bc4bd0c8235a
Andrey Manzharov <amanzharov@yandex.ru>
2024-01-22 08:04:24 +0300

    neovide: do not remember window size

--- config/nvim/plugin/neovide.lua
+++ config/nvim/plugin/neovide.lua
@@ -6,6 +6,7 @@ vim.g.neovide_refresh_rate = 60
 vim.g.neovide_transparency = 0.9
 vim.g.neovide_cursor_trail_size = 0
 vim.g.neovide_cursor_vfx_mode = 'railgun'
+vim.g.neovide_remember_window_size = false
 vim.opt.guifont = 'Iosevka Term'
 
 local function set_scale_factor(scale)
