f9988c37d921c336aa8b1f4536c333f1c5ee2aea
Andrey Manzharov <amanzharov@yandex.ru>
2024-02-07 16:06:04 +0300

    nvim: move `luasnip` to first completion group

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -76,9 +76,9 @@ return {
         sources = cmp.config.sources(
           {
             { name = 'nvim_lsp' },
+            { name = 'luasnip' },
           },
           {
-            { name = 'luasnip' },
             {
               name = 'emoji',
               option = {
