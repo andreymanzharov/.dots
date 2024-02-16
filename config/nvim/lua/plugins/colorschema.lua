d3eb6b1e4a8d750de43527ece83c7023d952be72
Andrey Manzharov <amanzharov@yandex.ru>
2024-02-16 17:16:08 +0300

    kitty: remove transparent background

--- config/nvim/lua/plugins/colorschema.lua
+++ config/nvim/lua/plugins/colorschema.lua
@@ -5,7 +5,6 @@ return {
     priority = 1000,
     config = function()
       require 'catppuccin'.setup {
-        transparent_background = true,
         no_italic = true,
       }
       vim.cmd.colorscheme 'catppuccin'
