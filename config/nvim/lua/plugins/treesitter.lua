67373ca7b1ccfe9e1ebb8ea86b170b04bc4360aa
Andrey Manzharov <amanzharov@yandex.ru>
2024-02-29 11:51:00 +0300

    revert "nvim: remove unnecessary config function"
    This reverts commit 2b294d18aa283a85788f6a689231b08362405c4a.

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -7,41 +7,43 @@ return {
     dependencies = {
       { 'nvim-treesitter/nvim-treesitter-textobjects' }
     },
-    opts = {
-      ensure_installed = { 'c', 'cpp', 'go', 'java', 'lua', 'python', 'rust', 'vim', 'zig' },
-      highlight = {
-        enable = true,
-        disable = {},
-      },
-      textobjects = {
-        select = {
+    config = function()
+      require 'nvim-treesitter.configs'.setup {
+        ensure_installed = { 'c', 'cpp', 'go', 'java', 'lua', 'python', 'rust', 'vim', 'zig' },
+        highlight = {
           enable = true,
-          keymaps = {
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = '@class.inner'
-          }
+          disable = {},
         },
-        swap = {
-          enable = true,
-          swap_next = {
-            ['<leader>L'] = '@parameter.inner',
+        textobjects = {
+          select = {
+            enable = true,
+            keymaps = {
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              ['ac'] = '@class.outer',
+              ['ic'] = '@class.inner'
+            }
           },
-          swap_previous = {
-            ['<leader>H'] = '@parameter.inner',
-          }
-        },
-        move = {
-          enable = true,
-          goto_next_start = {
-            [']]'] = '@function.outer',
+          swap = {
+            enable = true,
+            swap_next = {
+              ['<leader>L'] = '@parameter.inner',
+            },
+            swap_previous = {
+              ['<leader>H'] = '@parameter.inner',
+            }
           },
-          goto_previous_start = {
-            ['[['] = '@function.outer',
+          move = {
+            enable = true,
+            goto_next_start = {
+              [']]'] = '@function.outer',
+            },
+            goto_previous_start = {
+              ['[['] = '@function.outer',
+            },
           },
-        },
+        }
       }
-    },
+    end
   }
 }
