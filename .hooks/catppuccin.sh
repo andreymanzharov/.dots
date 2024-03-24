53bc229f774500b40e5a915eb5076b7483ee809b
Andrey Manzharov <amanzharov@yandex.ru>
2024-03-24 10:22:35 +0300

    bat: update `catppuccin` theme

--- .hooks/catppuccin.sh
+++ .hooks/catppuccin.sh
@@ -3,11 +3,11 @@ set -euo pipefail
 
 typeset -A themes
 themes=(
-  alacritty/catppuccin-mocha.toml     alacritty/main/catppuccin-mocha.toml
-  waybar/mocha.css                    waybar/main/themes/mocha.css
-  bat/themes/Catppuccin-mocha.tmTheme bat/main/Catppuccin-mocha.tmTheme
-  kitty/mocha.conf                    kitty/main/themes/mocha.conf
-  git/mocha.gitconfig                 delta/main/themes/mocha.gitconfig
+  alacritty/catppuccin-mocha.toml      alacritty/main/catppuccin-mocha.toml
+  waybar/mocha.css                     waybar/main/themes/mocha.css
+  bat/themes/Catppuccin\ Mocha.tmTheme bat/main/themes/Catppuccin%20Mocha.tmTheme
+  kitty/mocha.conf                     kitty/main/themes/mocha.conf
+  git/mocha.gitconfig                  delta/main/themes/mocha.gitconfig
 )
 
 for file in ${(k)themes}; do
