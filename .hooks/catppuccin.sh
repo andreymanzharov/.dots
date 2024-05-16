0f692a6268a95fc62117d244f6da6bcb07bee864
Andrey Manzharov <amanzharov@yandex.ru>
2024-05-16 11:22:16 +0300

    add support for OS specific configurations

--- .hooks/catppuccin.sh
+++ .hooks/catppuccin.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 typeset -A themes
 themes=(
   alacritty/catppuccin-mocha.toml      alacritty/main/catppuccin-mocha.toml
-  waybar/mocha.css                     waybar/main/themes/mocha.css
+  waybar.linux/mocha.css               waybar/main/themes/mocha.css
   bat/themes/Catppuccin\ Mocha.tmTheme bat/main/themes/Catppuccin%20Mocha.tmTheme
   kitty/mocha.conf                     kitty/main/themes/mocha.conf
   git/catppuccin.gitconfig             delta/main/catppuccin.gitconfig
