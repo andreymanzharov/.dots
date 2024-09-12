943f298fdb70f689450bfe9051633877ebe9b63c
Andrey Manzharov <amanzharov@yandex.ru>
2024-09-12 15:46:46 +0300

    nvim: make telescope lazy again

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -7,67 +7,77 @@ return {
       { 'nvim-telescope/telescope-ui-select.nvim' }
     },
     keys = {
-      { '<c-n>',            function() require 'telescope.builtin'.find_files() end },
-      { '<c-e>',            function() require 'telescope.builtin'.buffers() end },
-      { '<leader>ts',       function() require 'telescope.builtin'.treesitter() end },
-      { '<leader>h',        function() require 'telescope.builtin'.help_tags() end },
-      { '<leader>lg',       function() require 'telescope.builtin'.live_grep() end },
-      { '<leader><leader>', function() require 'telescope.builtin'.git_files() end },
-      { '<leader>gl',       function() require 'telescope.builtin'.git_commits() end },
-
-      {
-        '<leader>ev',
-        function()
-          require 'telescope.builtin'.find_files {
-            cwd = vim.fn.stdpath 'config',
-          }
-        end
-      },
+      '<c-n>',
+      '<c-e>',
+      '<leader>ts',
+      '<leader>h',
+      '<leader>lg',
+      '<leader><leader>',
+      '<leader>gl',
+      '<leader>gb',
+      '<leader>ev',
     },
-    opts = {
-      defaults = {
-        path_display = { "filename_first" },
-        mappings = {
-          i = {
-            ['<c-j>'] = 'move_selection_next',
-            ['<c-k>'] = 'move_selection_previous'
-          }
-        },
-        winblend = 15,
-      },
-      extensions = {
-        ['ui-select'] = {
-          require 'telescope.themes'.get_dropdown {}
-        }
-      },
-      pickers = {
-        buffers = {
+    config = function()
+      local telescope = require 'telescope'
+      local themes = require 'telescope.themes'
+
+      telescope.setup {
+        defaults = {
+          path_display = { "filename_first" },
           mappings = {
             i = {
-              ['<c-d>'] = 'delete_buffer',
-            },
-            n = {
-              ['<c-d>'] = 'delete_buffer',
+              ['<c-j>'] = 'move_selection_next',
+              ['<c-k>'] = 'move_selection_previous'
             }
-          }
+          },
+          winblend = 15,
         },
-        git_branches = {
-          mappings = {
-            i = {
-              ['<c-d>'] = 'git_delete_branch'
-            },
-            n = {
-              ['<c-d>'] = 'git_delete_branch'
+        extensions = {
+          ['ui-select'] = themes.get_dropdown {}
+        },
+        pickers = {
+          buffers = {
+            mappings = {
+              i = {
+                ['<c-d>'] = 'delete_buffer',
+              },
+              n = {
+                ['<c-d>'] = 'delete_buffer',
+              }
+            }
+          },
+          git_branches = {
+            mappings = {
+              i = {
+                ['<c-d>'] = 'git_delete_branch'
+              },
+              n = {
+                ['<c-d>'] = 'git_delete_branch'
+              }
             }
           }
         }
       }
-    },
-    config = function(_, opts)
-      local telescope = require 'telescope'
-      telescope.setup(opts)
+
       telescope.load_extension 'fzy_native'
       telescope.load_extension 'ui-select'
+
+      local builtin = require 'telescope.builtin'
+
+      vim.keymap.set('n', '<c-n>', builtin.find_files)
+      vim.keymap.set('n', '<c-e>', builtin.buffers)
+      vim.keymap.set('n', '<leader>ts', builtin.treesitter)
+      vim.keymap.set('n', '<leader>h', builtin.help_tags)
+      vim.keymap.set('n', '<leader>lg', builtin.live_grep)
+      vim.keymap.set('n', '<leader><leader>', builtin.git_files)
+      vim.keymap.set('n', '<leader>gl', builtin.git_commits)
+      vim.keymap.set('n', '<leader>gb', builtin.git_branches)
+
+      local function config_files()
+        builtin.find_files { cwd = vim.fn.stdpath 'config' }
+      end
+
+      vim.keymap.set('n', '<leader>ev', config_files)
     end
   }
 }
